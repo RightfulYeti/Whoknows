d559ce09ac1443d15f964e0ef5fd0874e5b7cb08
Jeppe Willatzen <JeppeWi@outlook.com>
2020-02-19 14:21:16 +0100

    Planet Info, Planet Status Bars, Position Fix
    Planet Info, Planet Status Bars, Position Fix

---
+++ Planet Game/Assets/Color.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color : MonoBehaviour
+{
+
+    public float speed;
+    public UnityEngine.Color startColor;
+    public UnityEngine.Color endColor;
+    public bool repeatable = false;
+    float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!repeatable)
+        {
+            float t = (Time.time - startTime) * speed;
+            GetComponent<Renderer>().material.color = UnityEngine.Color.Lerp(startColor, endColor, t);
+        }
+        else
+        {
+            float t = (Mathf.Sin(Time.time - startTime) * speed);
+            GetComponent<Renderer>().material.color = UnityEngine.Color.Lerp(startColor, endColor, t);
+        }
+    }
+}
---
+++ Planet Game/Assets/Scripts/Planets/BarScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarScript : MonoBehaviour
+{
+    public GameObject Water;
+    public GameObject Food;
+    public GameObject Money;
+    Vector3 WaterLocalScale;
+    Vector3 FoodLocalScale;
+    Vector3 MoneyLocalScale;
+    public Planet Planet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WaterLocalScale = Water.transform.localScale;
+        FoodLocalScale = Food.transform.localScale;
+        MoneyLocalScale = Money.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WaterLocalScale.x = Planet.Water;
+        Water.transform.localScale = WaterLocalScale;
+
+        FoodLocalScale.x = Planet.Food;
+        Food.transform.localScale = FoodLocalScale;
+
+        MoneyLocalScale.x = Planet.Money;
+        Money.transform.localScale = MoneyLocalScale;
+    }
+}

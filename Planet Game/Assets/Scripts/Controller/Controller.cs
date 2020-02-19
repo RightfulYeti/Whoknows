a73c2874215802c6f7e121e2d4151784fa4cb03b
Jeppe Willatzen <JeppeWi@outlook.com>
2020-02-19 13:09:12 +0100

    Planets setup, Initial scripts
    - 3 Planets in Scene with prefabs
    - Initial scripts for overall PlanetInfo & individual planets
    - Basic resources script

---
+++ Planet Game/Assets/Scripts/Controller/Controller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    Ray Ray;
+    RaycastHit Hit;
+    public PlanetsInfo PlanetInfoRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Planet click detection
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray = Camera.main.ScreenPointToRay(Input.mousePosition);     
+            if (Physics.Raycast(Ray, out Hit))
+            {
+                if (Hit.transform.tag == "Planet")
+                {
+                    PlanetInfoRef.PlanetFinder(Hit.transform.name);
+                }
+                else
+                {
+                }
+            }
+        }
+
+   
+    }
+
+}

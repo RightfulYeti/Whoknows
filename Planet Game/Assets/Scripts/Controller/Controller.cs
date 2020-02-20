bc5d09b0a13bbd6b90d4f25472434d09b8cc4a55
Jeppe Willatzen <JeppeWi@outlook.com>
2020-02-20 10:28:21 +0100

    Resource selection detection, Mouse release
    - Resource selection detection
    - Mouse release
    - Resource click class

--- Planet Game/Assets/Scripts/Controller/Controller.cs
+++ Planet Game/Assets/Scripts/Controller/Controller.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ResourceType {WATER, FOOD, MONEY};
+
+class ResourceInfo
+{
+    public Transform ParentPlanet;
+    public ResourceType RType;
+}
+
 public class Controller : MonoBehaviour
 {
     Ray Ray;
     RaycastHit Hit;
     public PlanetsInfo PlanetInfoRef;
-
+    private bool Selected = false;
 
     // Start is called before the first frame update
     void Start()
@@ -32,21 +40,45 @@ public class Controller : MonoBehaviour
                 }
             }
         }
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && !Selected)
         {
             Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(Ray, out Hit))
             {
                 if (Hit.transform.tag == "Resource")
                 {
-                    print("Holding button on Resource! ");
+                    if (Hit.transform.parent.tag == "Money Bar")
+                    {
+                        print("Holding button on a Money resource!");
+                        ResourceInfo ResourceAction = new ResourceInfo();
+                        ResourceAction.ParentPlanet = Hit.transform.parent.parent.parent;
+                        ResourceAction.RType = ResourceType.MONEY;
+                    }
+                    else if (Hit.transform.parent.tag == "Water Bar")
+                    {
+                        print("Holding button on a Water resource!");
+                        ResourceInfo ResourceAction = new ResourceInfo();
+                        ResourceAction.ParentPlanet = Hit.transform.parent.parent.parent;
+                        ResourceAction.RType = ResourceType.WATER;
+                    }
+                    else if (Hit.transform.parent.tag == "Food Bar")
+                    {
+                        print("Holding button on a Food resource!");
+                        ResourceInfo ResourceAction = new ResourceInfo();
+                        ResourceAction.ParentPlanet = Hit.transform.parent.parent.parent;
+                        ResourceAction.RType = ResourceType.FOOD;
+                    }
+                    Selected = true;
                 }
                 else
                 {
                 }
             }
         }
-
+        else if (Input.GetMouseButtonUp(0))
+        {
+            print("Released mouse button!");
+        }
 
     }
 

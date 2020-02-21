bc7b8f9bf0867f9e4d72b70ac9a375ad788c4c01
Jeppe Willatzen <JeppeWi@outlook.com>
2020-02-21 21:45:09 +0100

    Countdown Timer
    Countdown Timer

---
+++ Planet Game/Assets/Scripts/Game/Countdown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+    public float Timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Timer >= 0.0f)
+        {
+            print(Timer);
+            Timer -= Time.deltaTime;
+            int DisplayTimer = (int)Timer+1;
+            gameObject.GetComponent<Text>().text = DisplayTimer.ToString();
+        }
+        else if (Timer <= 0)
+        {
+            gameObject.GetComponent<Text>().enabled = false;
+        }
+    }
+}

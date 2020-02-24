4cc07b075102d4aacd20a7ef53d565358eb71537
Jeppe Willatzen <JeppeWi@outlook.com>
2020-02-24 21:00:05 +0100

    New Countdown Sprites
    New Countdown Sprites

--- Planet Game/Assets/Scripts/Game/Countdown.cs
+++ Planet Game/Assets/Scripts/Game/Countdown.cs
@@ -9,10 +9,19 @@ public class Countdown : MonoBehaviour
     public float CDTimer;
     private GameManager GameManagerRef;
     public bool Counting = true;
+    private int CurrentActiveUISprite;
+    public Image Image1;
+    public Image Image2;
+    public Image Image3;
+
     private void Start()
     {
         GameManagerRef = FindObjectOfType<GameManager>();
         CDTimer = Timer;
+        CurrentActiveUISprite = 1;
+        Image1.gameObject.SetActive(false);
+        Image2.gameObject.SetActive(false);
+        Image3.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,7 +33,8 @@ public class Countdown : MonoBehaviour
             if (Counting)
                 CDTimer -= Time.deltaTime;
             int DisplayTimer = (int)CDTimer + 1;
-            gameObject.GetComponent<Text>().text = DisplayTimer.ToString();
+            //gameObject.GetComponent<Text>().text = DisplayTimer.ToString();
+            CurrentActiveUISprite = DisplayTimer;
         }
         else if (CDTimer <= 0.0f)
         {
@@ -32,6 +42,29 @@ public class Countdown : MonoBehaviour
             gameObject.GetComponent<Text>().enabled = false;
             CDTimer = Timer;
             Counting = false;
+            CurrentActiveUISprite = 0;
+            Image1.gameObject.SetActive(false);
+            Image2.gameObject.SetActive(false);
+            Image3.gameObject.SetActive(false);
+        }
+
+        if (CurrentActiveUISprite == 1)
+        {
+            Image1.gameObject.SetActive(true);
+            Image2.gameObject.SetActive(false);
+            Image3.gameObject.SetActive(false);
+        }
+        else if (CurrentActiveUISprite == 2)
+        {
+            Image1.gameObject.SetActive(false);
+            Image2.gameObject.SetActive(true);
+            Image3.gameObject.SetActive(false);
+        }
+        else if (CurrentActiveUISprite == 3)
+        {
+            Image1.gameObject.SetActive(false);
+            Image2.gameObject.SetActive(false);
+            Image3.gameObject.SetActive(true);
         }
     }
 }

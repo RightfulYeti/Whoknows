55c15f1cef5b1dbb6f824e434e1aa608e451d342
Jeppe Willatzen <JeppeWi@outlook.com>
2020-02-26 23:22:10 +0100

    Crap
    Crap

--- Planet Game/Assets/Wrapper.cs
+++ Planet Game/Assets/Wrapper.cs
@@ -6,11 +6,15 @@ public class Wrapper : MonoBehaviour
 {
     public float BufferZone;
     private Camera Cam;
+    private TrailRenderer PlanetTrailRenderer;
+    private int VisibilitySwap = 0;
+    private float Timer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         Cam = Camera.main;
+        PlanetTrailRenderer = gameObject.GetComponent<TrailRenderer>();
     }
 
     // Update is called once per frame
@@ -18,47 +22,62 @@ public class Wrapper : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.X))
         {
-            if (gameObject.GetComponent<TrailRenderer>().emitting)
-                gameObject.GetComponent<TrailRenderer>().emitting = false;
+            if (PlanetTrailRenderer.emitting)
+                PlanetTrailRenderer.emitting = false;
             else
-                gameObject.GetComponent<TrailRenderer>().emitting = true;
+                PlanetTrailRenderer.emitting = true;
         }
+
+
+        //if (VisibilitySwap == 1)
+        //{
+        //    Timer += Time.deltaTime;
+        //}
+        //if (Timer >= 0.50f && VisibilitySwap == 0)
+        //{
+        //    PlanetTrailRender.emitting = true;
+        //    Timer = 0.0f;
+        //}
     }
 
     void OnBecameInvisible()
     {
-        gameObject.GetComponent<TrailRenderer>().emitting = false;
-        if (Cam != null)
+        //VisibilitySwap = 1;
+        print("Invisible!");
+        PlanetTrailRenderer.emitting = false;
+        if (Cam)
         {
-           // print(Cam.WorldToViewportPoint(transform.position));
+            // Screen left bounds
             if (Cam.WorldToViewportPoint(transform.position).x < 0)
             {
                 print("1");
-                transform.forward = transform.forward * -1;
-                //transform.position = new Vector3(transform.position.x * -1, transform.position.y, transform.position.z);
+                transform.position = new Vector3(transform.position.x * -1, transform.position.y, transform.position.z);
             }
+            // Screen right bounds
             else if (Cam.WorldToViewportPoint(transform.position).x > 1)
             {
                 print("2");
-                transform.forward = transform.forward * -1;
-                //transform.position = new Vector3(transform.position.x * -1, transform.position.y, transform.position.z);
+                transform.position = new Vector3(transform.position.x * -1, transform.position.y, transform.position.z);
             }
+            // Screen bottom bounds
             else if (Cam.WorldToViewportPoint(transform.position).y < 0)
             {
                 print("3");
-                transform.forward = transform.forward * -1;
             }
+            // Screen top bounds
             else if (Cam.WorldToViewportPoint(transform.position).y > 1)
             {
                 print("4");
-                transform.forward = transform.forward * -1;
             }
+            // This code is for ricochet effect / bouncing off the screen sides
+            //transform.forward = transform.forward * -1;
         }
     }
 
     private void OnBecameVisible()
     {
-        //print("Visible!");
-        gameObject.GetComponent<TrailRenderer>().emitting = true;
+        VisibilitySwap = 0;
+        print("Visible!");
+        PlanetTrailRenderer.emitting = true;
     }
 }

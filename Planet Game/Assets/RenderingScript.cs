8cf9c49a55a0bdd3a4aaa35fa234be9f25d28709
Jeppe Willatzen <JeppeWi@outlook.com>
2020-02-27 21:20:01 +0100

    Random Mesh materials for planets, slight size adjustment
    Random Mesh materials for planets, slight size adjustment

---
+++ Planet Game/Assets/RenderingScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderingScript : MonoBehaviour
+{
+    private MeshRenderer PlanetMesh;
+    public Material[] MaterialList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlanetMesh = GetComponent<MeshRenderer>();
+        int RandomMesh = Random.Range(0, 7);
+        PlanetMesh.material = MaterialList[RandomMesh];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
